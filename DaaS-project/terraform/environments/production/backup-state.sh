08fd581b31112ec9574aab941204a61df00bc96e
Tom Guenin <guenin.tom@gmail.com>
2026-01-23 12:29:42 +0100

    supperession DaaS

--- DaaS-project/terraform/environments/production/backup-state.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BACKUP_DIR=~/terraform-backups
-DATE=$(date +%Y%m%d-%H%M%S)
-cp /srv/samba/terraform/environments/production/terraform.tfstate \
-   $BACKUP_DIR/terraform.tfstate.$DATE
-
-# Rétention de 30 jours
-ls -t $BACKUP_DIR/terraform.tfstate.* | tail -n +31 | xargs rm -f
-echo "State backed up to $BACKUP_DIR/terraform.tfstate.$DATE"
